3b4c4c62b198a6265d0e4357dca01a29f1d19271
Rinat Urazaev <121090307+rinatzxc@users.noreply.github.com>
2023-04-21 23:36:48 +0300

    Add files via upload

---
+++ bloc_blog/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\R\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\dart\\bloc_blog" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\R\\flutter"
+  "PROJECT_DIR=C:\\dart\\bloc_blog"
+  "FLUTTER_ROOT=C:\\Users\\R\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\dart\\bloc_blog\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\dart\\bloc_blog"
+  "FLUTTER_TARGET=C:\\dart\\bloc_blog\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\dart\\bloc_blog\\.dart_tool\\package_config.json"
+)
